f2bb3be22c53b67171f20768d85850692a80f420
whitepcc <whitepcc@PCC-3000007593.pcn.pouchen.com.tw>
2018-08-08 14:17:29 +0800

    this is white

---
+++ maven-lab/src/main/java/DevOpsGroup/maven_lab/app_white.java
@@ -0,0 +1,8 @@
+package DevOpsGroup.maven_lab;
+
+public class app_white {
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "Hello I'm white" );
+    }
+}
