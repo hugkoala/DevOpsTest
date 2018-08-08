6c5a0fd25c96df11afc7fdbd96394886beea7b8d
Leo.Li <Leo.Li@PCC-3000005067.pcc.com.tw>
2018-08-08 13:32:00 +0800

    test

---
+++ maven-lab/src/main/java/DevOpsGroup/maven_lab/App.java
@@ -0,0 +1,13 @@
+package DevOpsGroup.maven_lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-lab/src/test/java/DevOpsGroup/maven_lab/DiffDemoTest.java
@@ -0,0 +1,11 @@
+package DevOpsGroup.maven_lab;
+
+public class DiffDemoTest {
+
+	public static void main(String[] args) throws Exception {
+		
+		DiffDemo diffDemo = new DiffDemo();
+        System.out.println(diffDemo.getDiff("2", "3"));
+	}
+
+}
