084c100f431a9ed03a90c550df6c77ad3bb01bb3
david.chin <david.chin@PCC-3000007031.pcn.pouchen.com.tw>
2018-08-08 14:01:20 +0800

    test add class

---
+++ maven-lab/src/main/java/DevOpsGroup/maven_lab/david_addtest.java
@@ -0,0 +1,8 @@
+package DevOpsGroup.maven_lab;
+
+public class david_addtest {
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "Hello I'm david" );
+    }
+}
